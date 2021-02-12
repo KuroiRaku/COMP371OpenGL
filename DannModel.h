42e62d3f7e6ac28f1056ed7e2938b00fe23f3e8f
Dann Kaoussarani <danjaf2@gmail.com>
2021-02-12 16:52:02 -0500

    DannModels Added

---
+++ DannModel.h
@@ -0,0 +1,28 @@
+#include <glad/glad.h>	// include GL Extension Wrangler
+#include <glfw/glfw3.h>	// include GLFW helper library
+#include <iostream>
+
+#pragma once
+class DannModel
+{
+
+public:
+	DannModel();
+	void drawModel();
+
+private:
+	void setLetterD(GLfloat xOrigin, GLfloat yOrigin, GLfloat zOrigin);
+	void setLetterN(GLfloat xOrigin, GLfloat yOrigin, GLfloat zOrigin);
+	void setNumber4(GLfloat xOrigin, GLfloat yOrigin, GLfloat zOrigin);
+	void setNumber7(GLfloat xOrigin, GLfloat yOrigin, GLfloat zOrigin);
+
+	GLuint vao_D;
+	GLuint vao_N;
+	GLuint vao_4;
+	GLuint vao_7;
+	GLfloat lineSize;
+	int indiciesD;
+	int indiciesN;
+	int indicies4;
+	int indicies7;
+};
