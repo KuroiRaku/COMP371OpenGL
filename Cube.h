b067ea29e0a94c489dbe9041f1bd1b1713761b11
Alessandro Kreslin <kalessandro14@gmail.com>
2021-03-17 11:29:03 -0400

    created texture class and added cube

---
+++ Cube.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <glad/glad.h>    
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+class Cube
+{
+
+public:
+    GLuint vao_Cube;
+    int indiciesC;
+    void drawModel();
+    Cube(GLfloat centerX, GLfloat centerY, GLfloat centerZ, GLfloat tallNess, GLfloat width, GLfloat length);
+    Cube() {};
+    void SetCube(GLfloat centerX, GLfloat centerY, GLfloat centerZ, GLfloat edgeLength, GLfloat tallNess, GLfloat width, GLfloat length);
+    glm::mat4 cubeMatrix = glm::mat4(1.0f);
+};
---
+++ Texture.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <glad/glad.h>	// include GL Extension Wrangler
+#include <glfw/glfw3.h>	// include GLFW helper library
+#include <glm/glm.hpp>
+#include <string>
+
+using namespace std;
+
+class Texture
+{
+private:
+	unsigned int m_RendererID;
+	string m_FilePath;
+	unsigned char* m_LocalBuffer;
+	int m_Width, m_Height, m_BPP;
+public:
+	Texture(const string& path);
+	~Texture();
+
+	void Bind(unsigned int slot = 0) const;
+	void Unbind() const;
+};
+
